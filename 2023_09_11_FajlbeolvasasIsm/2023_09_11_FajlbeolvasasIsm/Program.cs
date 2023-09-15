25646ae9704e0e2964da6751ab47cc62954d7d4a
Szabó Dániel <szabo.daniel@ckik.hu>
2023-09-15 08:19:57 +0200

    2023_09_15

---
+++ 2023_09_11_FajlbeolvasasIsm/2023_09_11_FajlbeolvasasIsm/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2023_09_11_FajlbeolvasasIsm
+{
+    class Program
+    {
+        static List<Adat> t = new List<Adat>();
+
+        static void Main(string[] args)
+        {
+            Fajlbeolvasas1();
+            Fajlbeolvasas2();
+            Fajlbeolvasas3();
+
+            Console.ReadLine();
+        }
+
+        private static void Fajlbeolvasas3()
+        {
+            try
+            {
+                StreamReader f = new StreamReader("megoldas.txt");
+                int db = Convert.ToInt32(f.ReadLine());
+                while (!f.EndOfStream)
+                {
+                    Adat a = new Adat();
+                    a.matrix = new int[10, 10];
+                    a.nev = f.ReadLine();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int[] szamok = Array.ConvertAll(f.ReadLine().Split(' '), Convert.ToInt32);
+                        for (int j = 0; j < szamok.Length; j++)
+                        {
+                            a.matrix[i, j] = szamok[j];
+                        }
+                    }
+                    t.Add(a);                        
+                }
+                f.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void Fajlbeolvasas2()
+        {
+            try
+            {
+                StreamReader f = new StreamReader("feladvany.txt");
+
+                while (!f.EndOfStream)
+                {
+                    string sor = f.ReadLine();
+                    string[] st = sor.Split(' ');
+                    //...
+                }
+
+                f.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private static void Fajlbeolvasas1()
+        {
+            try
+            {
+                string[] osszessor = File.ReadAllLines("feladvany.txt");
+            }
+            catch(FileNotFoundException e /*Exception e*/)
+            {
+                Console.WriteLine("A megadott fájl az adott útvonalon nem elérhető.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    struct Adat
+    {
+        public string nev;
+        public int[,] matrix;
+    }
+}
---
+++ 2023_09_11_Objektumok/2023_09_11_Objektumok/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_09_11_Objektumok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kutya k1 = new Kutya("Bütyök","barna", true);
+            Console.WriteLine(k1.ToString());
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class Kutya
+    {
+        // Jellemzői, adattagjai, mezői, tulajdonságai
+        string nev, szin, fajta;
+        int kor;
+        bool kan;
+
+        // Konstruktor (speciális metódus) - példányosításhoz
+        // public Kutya() { }
+
+        public Kutya(string nev, string szin, bool kan)
+        {
+            this.nev = nev;
+            this.szin = szin;
+            this.kan = kan;
+        }
+
+        // Metódusok - eljárások, függvények - feladatok
+        public void Ugatas()
+        {
+            Console.WriteLine("Vau-Vau");
+        }
+
+        // Getter, setter
+        public string GetNev() { return nev; }
+        public void SetNev(string ujnev) { nev = ujnev; }
+
+        public string Nev
+        {
+            get { return nev; }
+            private set { nev = value; }
+        }
+
+
+        static List<string> kartevok = new List<string>() { "bolha", "tetű" };
+
+        public string ToString()
+        {
+            return string.Format("{0} {1} {2} {3} {4}", nev, szin, fajta, kor, kan);
+        }
+
+    }
+}
