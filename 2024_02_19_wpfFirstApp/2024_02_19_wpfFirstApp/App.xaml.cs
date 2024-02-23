9fede588b4e359ecc8d749cc7a69346bb9df49bb
Szabó Dániel <szabo.daniel@ckik.loc>
2024-02-23 09:41:01 +0100

    2024_02_23

---
+++ 2024_02_19_wpfFirstApp/2024_02_19_wpfFirstApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _2024_02_19_wpfFirstApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Mutex instMutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool ujLetrehozasa;
+            instMutex = new Mutex(true, "egyedinev", out ujLetrehozasa);
+            if (!ujLetrehozasa)
+            {
+                MessageBox.Show("Az alkalmazás már fut egy példányban.");
+                Current.Shutdown();
+                return;
+            }
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ 2024_02_19_wpfFirstApp/2024_02_19_wpfFirstApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace _2024_02_19_wpfFirstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnLogin.Background = new SolidColorBrush(Properties.Settings.Default.Mycolor);
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //btnLogin.Background = Brushes.Green;
+
+            Properties.Settings.Default.Mycolor = Colors.Red;
+            Properties.Settings.Default.Save();
+            btnLogin.Background = new SolidColorBrush(Properties.Settings.Default.Mycolor);
+
+
+            IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream fs = new IsolatedStorageFileStream("mentesek.txt", FileMode.Create, FileAccess.Write, isf);
+
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(txtLogin.Text + " " + txtPass.Text);
+            
+            sw.Close();
+            fs.Close();
+
+            MessageBox.Show("OK");
+        }
+    }
+}
